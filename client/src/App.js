2927f06d6b343a76b44155d07a59bec0b3a26b55
Łukasze <lukaszkucharzyk17@gmail.com>
2023-07-17 14:06:48 +0200

    added routing & about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
+import { Home } from './components/pages/Home';
+import { About } from './components/pages/About';
+import { Fragment } from 'react';
 
 const App = ()=> {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <Router>
+      <Fragment>
+          <Navbar/>
+          <div className="container">
+              <Routes>
+                <Route exact path='/' element ={<Home/>}/>
+                <Route exact path='/about' element ={<About/>}/>
+              </Routes>
+          </div>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h1>About this app</h1>
+        <p className="my-1">
+            Full stack react app for contacts
+        </p>
+        <p className="bg-dark p">
+            <strong>Version: </strong> 1.0.0
+        </p>
+    </div>
+  )
+}
